5300828748e33b13a0886fa0f0fa83717f7df8f4
agrio15 <ashleeagres56@gmail.com>
2024-04-06 12:22:58 -0500

    primera parte

---
+++ src/main/java/pe/edu/cibertec/appformularios/AppformulariosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appformularios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppformulariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppformulariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appformularios/AppformulariosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appformularios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppformulariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
